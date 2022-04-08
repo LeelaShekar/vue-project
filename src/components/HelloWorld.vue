de3884448a64cc1416a601bbe43a07d3a71a2109
LeelaShekar <44741546+LeelaShekar@users.noreply.github.com>
2022-04-08 19:12:12 +0530

    test-files
    test-files

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,123 @@
+<template>
+<v-container class="pt-10">
+  <div class="v-form-wrap">
+  <h3 class="mb-3 text-center">Enter the details to Sign Up !</h3>
+  <v-form v-model="valid">
+    <v-container>
+      <v-row>
+        <v-col
+          cols="12"
+          md="12"
+        >
+          <v-text-field
+            v-model="firstname"
+            :rules="nameRules"
+            :counter="10"
+            label="First name"
+            required
+          ></v-text-field>
+        </v-col>
+
+        <v-col
+          cols="12"
+        md="12"
+        >
+          <v-text-field
+            v-model="lastname"
+            :rules="nameRules"
+            :counter="10"
+            label="Last name"
+            required
+          ></v-text-field>
+        </v-col>
+
+        <v-col
+          cols="12"
+        md="12"
+        >
+          <v-text-field
+            v-model="email"
+            :rules="emailRules"
+            label="E-mail"
+            required
+          ></v-text-field>
+        </v-col>
+        <v-col
+          cols="12"
+        md="12"
+        >
+          <v-text-field 
+            v-model="password"
+            :append-icon="show1 ? 'mdi-eye' : 'mdi-eye-off'"
+            :rules="[rules.required, rules.min]"
+            :type="show1 ? 'text' : 'password'"
+            name="input-10-1"
+            label="Password"
+            hint="At least 8 characters"
+            counter
+            @click:append="show1 = !show1"
+          ></v-text-field>
+        </v-col>
+          <v-col
+          cols="12" class="text-center"
+        md="12"
+        >
+         <v-btn
+        class="mr-4"
+        type="submit"
+        :disabled="invalid"
+      >
+        submit
+      </v-btn>
+          </v-col>
+      </v-row>
+    </v-container>
+  </v-form>
+  </div>
+</v-container>
+</template>
+ 
+
+<script>
+  export default {
+    data () {
+      return {
+        show1: false,
+        show2: true,
+        show3: false,
+        show4: false,
+        password: 'Password',
+        rules: {
+          required: value => !!value || 'Required.',
+          min: v => v.length >= 8 || 'Min 8 characters',
+          emailMatch: () => (`The email and password you entered don't match`),
+        },
+        valid: false,
+      firstname: '',
+      lastname: '',
+      nameRules: [
+        v => !!v || 'Name is required',
+        v => v.length <= 10 || 'Name must be less than 10 characters',
+      ],
+      email: '',
+      emailRules: [
+        v => !!v || 'E-mail is required',
+        v => /.+@.+/.test(v) || 'E-mail must be valid',
+      ],
+
+      }
+    },
+  }
+</script>
+
+
+<style scoped>
+.v-form-wrap{
+     max-width: 600px;
+    margin: 0 auto; 
+}
+.v-form{
+border: 1px solid #f0f0f0;
+ padding: 0 20px 20px;
+}
+</style>
